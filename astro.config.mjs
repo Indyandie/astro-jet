99fe28ce0bf3cf6bea7c877f8bbf97eed9d2bfbc
indyandie <1058837+Indyandie@users.noreply.github.com>
2022-07-04 23:07:16 -0500

    gh-pg deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// publicDir: './public',
-  root: 'build',
-  // outDir: './build',
-  // base: './'
+  site: 'https://indyandie.github.io',
+  base: '/astro-jet'
 });
